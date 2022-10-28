4934850130fb3f65fc91b703612d2c94c4e530fa
kurousa <r.takahashi.1026@gmail.com>
2022-10-28 22:16:09 +0900

    retrieve data and add props for results

--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -3,15 +3,17 @@ import Selector from "../components/Selector";
 import Title from "../components/Title";
 
 export default function TopPage(props) {
-
   return (
-    <div>
-      <Title />
-      <Selector
-        countriesJson={props.countriesJson}
-        setCountry={props.setCountry}
-        getCountry={props.getCountry} />
-      <Results />
+    <div className="top-page-container">
+      <div>
+        <Title />
+        <Selector
+          countriesJson={props.countriesJson}
+          setCountry={props.setCountry}
+          getCountryData={props.getCountryData}
+        />
+        <Results countryDate={props.countryDate}/>
+      </div>
     </div>
   )
 }
