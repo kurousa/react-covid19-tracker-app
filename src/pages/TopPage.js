843720bc9c90970a58b8971f82933ece6e73e6e1
kurousa <r.takahashi.1026@gmail.com>
2022-10-28 21:32:00 +0900

    add TopPage as page

---
+++ src/pages/TopPage.js
@@ -0,0 +1,11 @@
+import Selector from "../components/Selector";
+import Title from "../components/Title";
+
+export default function TopPage() {
+  return (
+    <div>
+      <Title />
+      <Selector />
+    </div>
+  )
+}
